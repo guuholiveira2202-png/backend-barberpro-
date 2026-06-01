db10516e318c19f9a7bf70ae6cf29ba03d9b64ad
guuholiveira2202-png <guuholiveira2202@gmail.com>
2026-06-01 01:16:10 -0300

    back

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', // Sem senha no XAMPP
-    database: 'barberpro'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'barberpro',
+    port: process.env.DB_PORT || 3306
 });
 
 module.exports = db;
